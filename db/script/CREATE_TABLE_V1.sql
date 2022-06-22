4ebd0331cb9486c26e204fecfc948c3922397a60
linux <linux@example.com>
2022-06-22 12:34:47 +0200

    Updated script DDL

--- db/script/CREATE_TABLE_V1.sql
+++ db/script/CREATE_TABLE_V1.sql
@@ -1,3 +1,24 @@
-create user "user-mgmt-user" with encrypted password 'user-mgmt-user'; 
-create database "user-mgmt" with owner "user-mgmt-user";
-grant all privileges on database "user-mgmt" to "user-mgmt-user";
+-- Drop table
+
+DROP TABLE public.users;
+
+CREATE TABLE public.users (
+    id int8 NOT NULL,
+    address varchar(100) NULL,
+    email varchar(50) NOT NULL,
+    name varchar(50) NOT NULL,
+    "password" varchar(50) NOT NULL,
+    surname varchar(50) NOT NULL,
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+);
+
+CREATE UNIQUE INDEX users_pkey ON public.users USING btree (id);
+
+DROP SEQUENCE public.user_sequence;
+
+CREATE SEQUENCE public.user_sequence
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1
+    NO CYCLE;
