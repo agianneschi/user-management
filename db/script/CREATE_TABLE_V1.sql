ea4911564a73d59eb28ea38e38987b7e54786e1f
linux <linux@example.com>
2022-06-23 10:19:04 +0200

    Fix script DDL

--- db/script/CREATE_TABLE_V1.sql
+++ db/script/CREATE_TABLE_V1.sql
@@ -1,20 +1,20 @@
 -- Drop table
 
-DROP TABLE public.users;
+DROP TABLE IF EXISTS public.users;
 
 CREATE TABLE public.users (
-    id int8 NOT NULL,
-    address varchar(100) NULL,
-    email varchar(50) NOT NULL,
-    name varchar(50) NOT NULL,
-    "password" varchar(50) NOT NULL,
-    surname varchar(50) NOT NULL,
-    CONSTRAINT users_pkey PRIMARY KEY (id)
+	    id int8 NOT NULL,
+	    address varchar(100) NULL,
+	    email varchar(50) NOT NULL,
+	    name varchar(50) NOT NULL,
+	    "password" varchar(50) NOT NULL,
+	    surname varchar(50) NOT NULL,
+	    CONSTRAINT users_pkey PRIMARY KEY (id)
 );
 
-CREATE UNIQUE INDEX users_pkey ON public.users USING btree (id);
+CREATE UNIQUE INDEX IF NOT EXISTS users_pkey ON public.users USING btree (id);
 
-DROP SEQUENCE public.user_sequence;
+DROP SEQUENCE IF EXISTS public.user_sequence;
 
 CREATE SEQUENCE public.user_sequence
     INCREMENT BY 1
